8f33ebec18b714a2c1796a3e2e99a8b9e3a230fd
King_LATIF <62727720+king667-Latif@users.noreply.github.com>
2020-10-01 22:06:27 +0000

    Add files via upload

---
+++ rac/jsjs.js
@@ -0,0 +1,15 @@
+function redirection1(){
+	window.location.assign('https://www.google.com');
+}
+
+function redirection2(){
+	window.location.assign('https://www.facebook.com');
+}
+
+function redirection3(){
+	window.location.assign('https://www.youtube.com');
+}
+
+function redirection4(){
+	window.location.assign('https://www.instagram.com');
+}
